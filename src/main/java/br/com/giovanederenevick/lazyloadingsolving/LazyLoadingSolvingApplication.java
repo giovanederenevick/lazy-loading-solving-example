84fc4a196825fec0764ba0d1fec0a870d7986f70
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-10-04 22:29:45 -0300

    Initial commit

---
+++ src/main/java/br/com/giovanederenevick/lazyloadingsolving/LazyLoadingSolvingApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovanederenevick.lazyloadingsolving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyLoadingSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyLoadingSolvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/giovanederenevick/lazyloadingsolving/LazyLoadingSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanederenevick.lazyloadingsolving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyLoadingSolvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
